ca87cc645cc9ab40f83c942d95f37253a23c1b98
Laureano <laau262@gmail.com>
2022-04-28 20:00:16 -0300

    Se modificó index.html con las referencias a login y tamb header.js

--- base_js/header.js
+++ base_js/header.js
@@ -1,10 +1,10 @@
 const body = document.querySelector(".sticky");
 body.innerHTML = `
         <div class="sticky__header">
-            <a href="index.html"><p class="header__logo"></p></a>
+            <a href="../index.html"><p class="header__logo"></p></a>
             <div class="header__conjuntobusqueda">
                 <input class="header__buscador" type="text" placeholder="¿Que deseas buscar?">  
                 <i class="fa-solid fa-magnifying-glass"></i>
             </div>
-            <div class="header__login">Login</div>
+            <a href="screens/crud.html" class="header__login">Login</a>
         </div>`
