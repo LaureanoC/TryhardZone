7f35f10774cd6d94cdca935ebcd417ecd21885cd
Laureano <laau262@gmail.com>
2022-04-29 14:04:38 -0300

    Se añadió READ, Redireccionamiento dentro del crud. Falta CUD

--- base_js/head.js
+++ base_js/head.js
@@ -10,7 +10,6 @@ head.innerHTML = `
     <link rel="stylesheet" href="../assets/css/banner.css">
     <link rel="stylesheet" href="../assets/css/categorias.css">
     <link rel="stylesheet" href="../assets/css/footer.css">
-    <script src="https://kit.fontawesome.com/b95dc486b7.js" crossorigin="anonymous"></script>
     <link rel="apple-touch-icon" sizes="180x180" href="../assets/img/favicon/apple-touch-icon.png">
     <link rel="icon" type="image/png" sizes="32x32" href="../assets/img/favicon/favicon-32x32.png">
     <link rel="icon" type="image/png" sizes="16x16" href="../assets/img/favicon/favicon-16x16.png">
--- controllers/iniciarSesion.js
+++ controllers/iniciarSesion.js
@@ -1,4 +1,112 @@
 import { userServices } from "../service/user-service.js";
+import { productoServices } from "../service/product-service.js";
+
+const devolverTipo = (tipoSeccion) => {
+
+    if (tipoSeccion == "v"){
+        return "Videojuegos"
+    }
+    if (tipoSeccion == "p"){
+        return "Peluches"
+    }
+    if (tipoSeccion == "c"){
+        return "Camisetas Esports"
+    }
+
+}
+
+const crearBotonReferencia = (ref) =>{
+
+    /* <a href="#videojuegos"><div class="banner__boton">Ver videojuegos</div></a>*/
+    const d = document.createElement("div");
+    d.className = "botonRedireccion";
+    d.innerHTML = `Ir a ${devolverTipo(ref)}`
+    const a = document.createElement("a");
+    a.setAttribute("href",`#${ref}`);
+    const main = document.querySelector(".redireccion");
+    a.appendChild(d);
+    main.appendChild(a);
+
+
+}
+
+const crearSection = (tipoSeccion) => {
+
+    let titulo = devolverTipo(tipoSeccion);
+   
+    const referencia = document.createElement("a");
+    referencia.setAttribute("name",`${tipoSeccion}`);
+    const section = document.createElement("section");
+        section.className = "categorias";
+        const div = document.createElement("div");
+        div.className = "categorias__contenido";
+        div.setAttribute(`data-section${tipoSeccion}`,``);
+    const div2 = document.createElement("div");
+            div2.className = "categorias__header";
+    const h2 = document.createElement("h2");
+        h2.className = "categorias__titulo";
+        h2.innerHTML = `${titulo}`;
+    const link = document.createElement("a");
+        link.className = "categorias__link";
+        link.innerHTML = `Agregar producto <i class="fa-solid fa-plus"></i>`;
+
+        link.addEventListener("click", () => {
+
+
+            console.log("Haz hecho click en la categoria ", titulo);
+        })
+
+
+    const main = document.querySelector(".espaciado");
+    div2.appendChild(h2);
+    div2.appendChild(link);
+    div.appendChild(div2);
+    section.appendChild(div);
+    main.appendChild(referencia);
+    main.appendChild(section);
+    
+} 
+
+const crearNuevaCard = (nombre, precio, imagen) => {
+    const card = document.createElement("div");
+    card.className = "categorias__card"
+    const contenido = `
+        <img class="card__img"src="${imagen}">
+        <p class="card__nombre">${nombre}</p>
+        <p class="card__precio">${precio}</p>
+        <a class="card__link" href="#">Ver producto</a>    
+    `;
+    card.innerHTML = contenido;
+    return card
+}
+
+const mostrarProductos = () => {
+
+    const flex = document.createElement("div");
+    flex.className="redireccion"
+    const m = document.querySelector(".espaciado");
+    m.appendChild(flex);
+    
+    crearBotonReferencia("v");
+    crearBotonReferencia("c");
+    crearBotonReferencia("p");
+
+    crearSection("v");
+    crearSection("c");
+    crearSection("p");
+
+    
+    
+    productoServices.listaProducto().then((data) => {
+        console.log(data);
+        data.forEach( ({nombre,precio,imagen,tipo}) => {
+            const div = document.querySelector(`[data-section${tipo}]`);
+            const nuevoProducto = crearNuevaCard(nombre,precio,imagen);
+            div.appendChild(nuevoProducto);
+        })
+    })
+console.log("xd");
+}
 
 const logout = () => { 
     userServices.perfilUsuario().then((data) => {    
@@ -54,6 +162,7 @@ const verificarEstado = () => {
                 })
             } else {
                 cambiarLogOutALogin();
+                mostrarProductos();
             }
         });
 }
