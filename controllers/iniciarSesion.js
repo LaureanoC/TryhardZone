38c9950844cb0710a5bc9838066d6fb7c3bfac8f
Laureano <laau262@gmail.com>
2022-04-29 00:30:28 -0300

    Se agregó funcionalidad de login a la pagina de crud

--- controllers/iniciarSesion.js
+++ controllers/iniciarSesion.js
@@ -1,45 +1,87 @@
 import { userServices } from "../service/user-service.js";
 
-const crearTemplate = () => {
 
-
-
+const cambiarLogOutALogin = () => {
+    const headerLogout = document.querySelector(".header__login");
+    headerLogout.innerHTML = "Cerrar sesión";
+    headerLogout.style.color = "red";
+    headerLogout.style.border = "solid red 1px";
+    headerLogout.removeAttribute("href");
+    headerLogout.addEventListener("click", ()=> {
+        logout();
+    })
 }
 
-const login = (u, p) => {
+const verificarEstado = () => {
+        userServices.perfilUsuario().then((data) => {
+            /*console.log(data);
+            console.log(data[0]);
+            console.log(data[0].estado);*/
+            let estado = data[0].estado;
+            console.log("el estado es ",estado);
 
+            if (estado == "false"){
+                /*Mostrar formulario*/
+                console.log("xD")
+                const main = document.querySelector("main");
+                main.innerHTML = `   
+                <section class="login">
+                <h1 class="login__titulo">Iniciar Sesión</h1>
+                <form class="login__form">
+                    <input name="usuario" type="text" class="form__usuario" id="usuario" placeholder="Escriba su usuario" data-usuario>
+                    <input name="password" type="password" class="form__password" id="password" placeholder="Escriba su contraseña" data-password>
+                    <button class="form__login">Ingresar</button>
+                </form>
+            </section>`
+                const btnInicioSesion = document.querySelector(".form__login");
+                console.log(btnInicioSesion);
+                btnInicioSesion.addEventListener("click", (evento)=> {
+                    evento.preventDefault();
+                    const user = document.querySelector("[data-usuario]").value;
+                    const pass = document.querySelector("[data-password]").value;
+                    console.log(user, pass);
+                    login(user,pass);
+                    console.log(user, pass);
+                })
+            
+            
+            
+            } else {
+                cambiarLogOutALogin();
+            }
+        });
+}
+
+const logout = () => { 
+    userServices.perfilUsuario().then((data) => {    
+        data.forEach(() => {
+                    userServices.actualizarEstado(1,false);                   
+        })
+        
+    })
+}
+
+
+verificarEstado();
+
+const login = (u, p) => {
     userServices.perfilUsuario().then((data) => {
         console.log(data);
-
         data.forEach(({usuario, password}) => {
+            console.log(usuario);
+            console.log(password);
             if(usuario == u){
-                if(password == p){
+                if(password == p){                   
+                    userServices.actualizarEstado(1,true);
                     console.log("true"); //Aquí debo hacer una petición para modificar el estado del usuario tamb
                                         // debo agregar cerrar sesión para que tenga sentido
                 } 
-            }
+            }     
         })
         
     })
     
 }
 
-const btnInicioSesion = document.querySelector(".form__login");
-console.log(btnInicioSesion);
 
-btnInicioSesion.addEventListener("click", (evento)=> {
-
-    evento.preventDefault();
-    const user = document.querySelector("[data-usuario]").value;
-    const pass = document.querySelector("[data-password]").value;
-
-    console.log(user, pass);
-
-    login(user,pass);
-
-    console.log(user, pass);
-
-    
-
-})
 
--- service/user-service.js
+++ service/user-service.js
@@ -1,6 +1,23 @@
-const perfilUsuario = () => fetch("http://localhost:3000/usuario").then((respuesta) => respuesta.json());
-console.log(perfilUsuario);
+const perfilUsuario = () => {
+    return fetch("http://localhost:3000/usuario").then((respuesta) => respuesta.json());
+}
+
+const actualizarEstado = (id,status) => {
+    
+    return fetch(`http://localhost:3000/usuario/${id}`,{
+        method:'PUT',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({estado: `${status}`,
+        usuario: "valentina",
+        password: "ponona pononita"})
+
+    })
+    .then((respuesta) => console.log(respuesta.json()))
+    .catch((error) => console.log(error));
+}
 
 export const userServices = {
-    perfilUsuario
+    perfilUsuario, actualizarEstado
 }
