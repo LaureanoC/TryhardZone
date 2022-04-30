bc227c8aac6dfa6d3be85b3f5b8d7c86b936e05c
Laureano <laau260@gmail.com>
2022-04-30 17:30:49 -0300

    Se añadieron estilos al formulario

--- base_js/footer.js
+++ base_js/footer.js
@@ -10,16 +10,24 @@ footer.innerHTML = `
         <a href="#">Quiero ser franquiciado</a>
         <a href="#">Anuncie aquí</a>
     </div>
-    <div class="footer__formulario">
-        <p class="formulario__titulo">Hable con nosotros</p>
-        <div class="formulario__conjunto">
-            <label class="formulario__label"for="name">Nombre</label>
-            <input name="name" type="text" class="formulario__nombre" id="nombre" placeholder="Laureano Neyén Chaves">
+
+    <form  class="footer__formulario" action="https://formspree.io/f/xdobppoy"method="POST">  
+              
+    <p class="formulario__titulo">Hable con nosotros</p>
+        <div class="formulario__conjunto" id="cajan">
+                <label class="formulario__label"for="name">Nombre y Appelido</label>
+                <input name="name" type="text" class="formulario__nombre" id="nombre" placeholder="Laureano Neyén Chaves" required autocomplete="off">
         </div>
-        <textarea name="mensaje" id="mensaje" class="formulario__mensaje" placeholder="Escribe tu mensaje"></textarea>
-        <div class="formulario__boton">Enviar</div>
-    </div>
-    
+
+        <div class="formulario__conjunto" id="caja">
+
+            <textarea name="mensaje" id="mensaje" class="formulario__mensaje" placeholder="Escribe tu mensaje" required></textarea>
+        </div>
+            
+            <button class="formulario__boton">Enviar</div>
+    </form>
+
 </div>
+   
 
 <div class="rodapie">Diseñado por Laureano Neyén Chaves 2022 &copy;</div>`
