42375a5699fbf2ce7682c7d2d548e41d93b04024
Laureano <laau260@gmail.com>
2022-05-01 21:01:21 -0300

    Error de tipeo

--- controllers/CRUD.js
+++ controllers/CRUD.js
@@ -644,7 +644,7 @@ const login = (u, p) => {
             console.log(password);
             if(usuario == u){
                 if(password == p){                   
-                    userServices.actualizarEstado(1,true).then(window.location(reload));
+                    userServices.actualizarEstado(1,true).then(window.location.reload());
                     console.log("true"); //Aquí debo hacer una petición para modificar el estado del usuario tamb
                                         // debo agregar cerrar sesión para que tenga sentido
                 } 
